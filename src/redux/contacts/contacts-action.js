bad03118eb046a9a72fbf2f39843970dc57c2523
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-10-07 15:15:06 +0300

    actions refactored with toolkits

--- src/redux/contacts/contacts-action.js
+++ src/redux/contacts/contacts-action.js
@@ -1,23 +1,43 @@
+//with Redux Toolkit
+import { createAction } from '@reduxjs/toolkit';
 import actionTypes from './contacts-types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const addContact = ({name,number}) => ({
-    type: actionTypes.ADD_CONTACT,
+export const addContact = createAction(actionTypes.ADD_CONTACT, ({name,number}) => ({
     payload: {
-      id: uuidv4(),
-      name,
-      number
-    }
-});
+        id: uuidv4(),
+        name,
+        number
+    }   
+}))
 
-export const deleteContact = contactId => ({
-    type: actionTypes.DELETE_CONTACT,
-    payload: contactId
-});
+export const deleteContact = createAction(actionTypes.DELETE_CONTACT);
+export const changeFilter = createAction(actionTypes.CHANGE_FILTER);
 
-export const changeFilter = value => ({
-    type: actionTypes.CHANGE_FILTER,
-    payload: value
-});
+    
+
+
+//without Redux Toolkit
+// import actionTypes from './contacts-types';
+// import { v4 as uuidv4 } from 'uuid';
+
+// export const addContact = ({name,number}) => ({
+//     type: actionTypes.ADD_CONTACT,
+//     payload: {
+//       id: uuidv4(),
+//       name,
+//       number
+//     }
+// });
+
+// export const deleteContact = contactId => ({
+//     type: actionTypes.DELETE_CONTACT,
+//     payload: contactId
+// });
+
+// export const changeFilter = value => ({
+//     type: actionTypes.CHANGE_FILTER,
+//     payload: value
+// });
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,22 @@
-import { createStore, combineReducers} from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+//with Redux Toolkit
+import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from './contacts/contacts-reducer'
 
-const rootReducer = combineReducers({
-    contacts: contactsReducer
+const store = configureStore({
+    reducer: {
+        contacts: contactsReducer
+    },
+    devTools: process.env.NODE_ENV === 'development',
 })
 
-const store = createStore(rootReducer, composeWithDevTools());
-
 export default store;
+
+//without Redux Toolkit
+//import { combineReducers } from "redux";
+//import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const rootReducer = combineReducers({
+//     contacts: contactsReducer
+// })
+
+//const store = createStore(rootReducer, composeWithDevTools());
